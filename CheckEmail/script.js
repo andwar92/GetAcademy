d8d8417adf9637dad3ae191d15bd16140f599844
andwar92 <andwar1992@gmail.com>
2025-03-14 14:25:16 +0100

    Update script.js

--- CheckEmail/script.js
+++ CheckEmail/script.js
@@ -24,15 +24,8 @@ function updateVeiw() {
 
 
 function saveEmail(inputText) {
-
     savedInput = inputText
-    // inputText.includes("@",".");
-    // if (savedInput = inputText){
-    // inputText.push(index,100<0)
-
-    // }
     updateVeiw();
-
 };
 
 function checkIfEmail(inputText) {
@@ -44,47 +37,5 @@ function checkIfEmail(inputText) {
             if ( inputText === filter.shouldNotContain[i]) {
             return "Ingen Email!"
             }       
-         }
-       
+        }     
 };   
-
-
-// for(let i =0; i<advices.length; i++){
-    //     if(i == index){
-    //         advice = advices[i];
-// function checkIfEmail(inputText) {
-//     let index = 0;
-//     let atIndex = -1; //Dette er for å senere sjekke om denne verdien har endret seg, slik at vi vet om @ er i inputText
-//     for (char of inputText) {
-//         if (char == filter.shouldContain[index] && atIndex == -1) {  //den ekstra sjekken er for å passe på at vi kun setter atIndex en gang
-//             
-//             console.log('Fant @ på index', atIndex)
-           
-//         }
-//         if( char === filter.shouldNotContain[index]){
-//             return "Ingen Email!"
-//         }
-//         if (atIndex !== -1 && index === atIndex + 1 && char === filter.shouldContain[1]) { //sjekker om atIndex har endret seg, at vi er på index'en etter @, og om elementet er "."
-//             console.log("Punktum er rett etter alfakrøll!");
-//             return "Email!";
-//         }
-//         index++;
-    
-//     }
-    
-
-//     return "Ingen Email!"
-    
-// };
-
-
-
-
-//     // if(atIndex!== -1 && inputText[atIndex+1]==filter.shouldContain[1]){
-//     //    console.log("inneholder punktum etter alfakrøll")
-//     //    return "email"; 
-//     // }
-    
-// // 
-
-
