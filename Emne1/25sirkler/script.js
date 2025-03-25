4e8cdfe2eaf06da0979039a419b6bb460a21aa2f
andwar92 <andwar1992@gmail.com>
2025-03-25 13:01:06 +0100

    head

--- Emne1/25sirkler/script.js
+++ Emne1/25sirkler/script.js
@@ -1,55 +1,68 @@
 //Model
 const app = document.getElementById('app')
-let secondsTimer;
+let countdownTime = 30*1000;
+let endTime = new Date().getTime() + countdownTime;
+let timeLeft = 10;
+let secondsTimer = '';
+let startTimer = 0;
+let endTimer = 0;
 let clicks = 0;
 //Veiw
 lampIndex();
 function updateVeiw() {
     app.innerHTML = /*HTML*/ `
-    <button onclick="start()">Start</button>
-    <div>
-    <h1>${clicks}
-    </div>
     <h1>Reaksjon Test!</h1>
     <div class="container">
-    ${reactionTest()}
+        ${Game()}
     </div>
+    Runde:
+    ${clicks}<br>
+    Tid per klikk:
+    ${secondsTimer}<br>
+    Tid igjen:
+    ${seconds}
+    <button onclick="start()"><h2>Start reaction test!</h2></button>
 `};
 
-function reactionTest() {
+function Game() {
     let reactionTest = '';
     for (let i = 0; i < 25; i++) {
-        console.log(selecredLampIndex == i)
         if (selecredLampIndex == i) {
-            console.log("is correct")
             reactionTest += `<div id="${i}" class="lightBulb lightOn" onclick ="randomLamp()"></div>`;
         }
         else {
             reactionTest += `<div id="${i}" class="lightBulb"></div>`;
         }
-
     }
+    startTimer = new Date().getTime();
     return reactionTest
 };
 
 //Controller
 function start() {
-    secondsTimer = 0;
-    for (let seconds = 0; seconds < 0; seconds++) {
-        if()
-    }
-        
-    // secondsTimer = clicks(1000);
+  let currentTime = new Date().getTime();
+  let timeLeft = endTime - currentTime;
+
+  if (timeLeft <= 0) {
+    clearInterval(timer);
+    altert("1-minute timer complete!");
+  } else {
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    console.log(`Time left: ${seconds} seconds`);
+  }
 };
-function timer() {
-    secondsTimer = 
-};
+
+
 function randomLamp() {
+    endTimer = new Date().getTime();
+    let milliSeonds = endTimer - startTimer;
+    secondsTimer = milliSeonds / 1000;
     clicks++;
     lampIndex();
 };
 
 function lampIndex() {
+    timer=setTimeout(Game,1000)
     selecredLampIndex = Math.floor(Math.random() * 25)
     updateVeiw();
 };
