675ffb195c880382824ddccfff74a4b6763be22b
andwar92 <andwar1992@gmail.com>
2025-03-31 10:18:51 +0200

    head

--- Emne1/25sirkler/model.js
+++ Emne1/25sirkler/model.js
@@ -1,18 +1,17 @@
 const model = {
-
     data: {
-        rounds: [],
         endTime: 0,
         countdownTime: 30 * 1000,
+        countdownTimer: 0,
+        countdownTime: 0,
         secondsTimer: '',
-        points: 0,
         timeLeft: 10,
         startTimer: 0,
         endTimer: 0,
+        rounds: [],
         round: 0,
         timeLeftMessage: '',
-        countdownTimer: 0,
-        countdownTime: 0,
         selectedLampIndex: 0,
+        points: 0,
     },
 }
